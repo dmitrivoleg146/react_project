7e987f0c1e03cfb8144140a8921aa3f27b338d3c
dmitrivoleg146 <dmitrivoleg146@gmail.com>
2022-02-02 14:41:25 +0200

    Homework + Errors

---
+++ 1-JS_lesson/1-JS_homework/script.js
@@ -0,0 +1,180 @@
+//1
+let a = "hello";
+let b = "owu";
+let c = "com";
+let d = "ua";
+let e = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let i = 3.14;
+let j = 2.7;
+let k = 16;
+let l = true;
+let m = false;
+
+console.log(a);
+alert(a);
+document.write(a + "<br>");
+
+
+console.log(b);
+alert(b);
+document.write(b + "");
+
+console.log(c);
+alert(c);
+document.write(c + "<br>");
+
+console.log(d);
+alert(d);
+document.write(d + "<br>");
+
+console.log(e);
+alert(e);
+document.write(e + "<br>");
+
+console.log(f);
+alert(f);
+document.write(f + "<br>");
+
+console.log(g);
+alert(g);
+document.write(g + "<br>");
+
+console.log(h);
+alert(h);
+document.write(h + "<br>");
+
+console.log(i);
+alert(i);
+document.write(i + "<br>");
+
+console.log(j);
+alert(j);
+document.write(j + "<br>");
+
+console.log(k);
+alert(k);
+document.write(k + "<br>");
+
+console.log(l);
+alert(l);
+document.write(l + "<br>");
+
+console.log(m);
+alert(m);
+document.write(m + "<br> <br>");
+
+//2
+a = "Jack";
+b = 0.67;
+c = -8.9;
+d = "Lviv";
+e = "Motors";
+f = 0;
+g = true;
+h = false;
+i = "color";
+j = "Morning";
+k = 45;
+l = -110;
+m = 4.567;
+
+console.log(a);
+alert(a);
+document.write(a + "<br>");
+
+console.log(b);
+alert(b);
+document.write(b + "<br>");
+
+console.log(c);
+alert(c);
+document.write(c + "<br>");
+
+console.log(d);
+alert(d);
+document.write(d + "<br>");
+
+console.log(e);
+alert(e);
+document.write(e + "<br>");
+
+console.log(f);
+alert(f);
+document.write(f + "<br>");
+
+console.log(g);
+alert(g);
+document.write(g + "<br>");
+
+console.log(h);
+alert(h);
+document.write(h + "<br>");
+
+console.log(i);
+alert(i);
+document.write(i + "<br>");
+
+console.log(j);
+alert(j);
+document.write(j + "<br>");
+
+console.log(k);
+alert(k);
+document.write(k + "<br>");
+
+console.log(l);
+alert(l);
+document.write(l + "<br>");
+
+console.log(m);
+alert(m);
+document.write(m + "<br><br>");
+
+//3
+let firstName = "Oleg";
+let middleName = "Andriovich";
+let lastName = "Dmutriv";
+let person = firstName + " " + middleName + " " + lastName;
+document.write(person + "<br>");
+document.write("<br>")
+
+//4
+let name = prompt("Enter your name");
+let surname = prompt("Your surname");
+let age = prompt("Your age");
+document.write("Вітаю" + " " + name + " " + surname + ". " + "Тобі" + " " + age + " " + "рік.");
+
+//5
+let aa = 100;
+let bb = '100';
+let cc = true;
+console.log(typeof aa);
+console.log(typeof bb);
+console.log(typeof cc);
+
+//6
+console.log(5 < 6);
+console.log(5 > 6);
+console.log(5 == 6);
+console.log(5 >= 6);
+console.log(10 >= 10);
+console.log(10 <= 10);
+console.log(10 != 10);
+console.log(10 < 10);
+console.log(10 > 10);
+console.log(123 === '123');
+console.log(123 !== '123');
+
+//7
+let str = "20";
+let aaa = 5 ;
+document.write(str + aaa + "<br/>"); //  205
+document.write(str - aaa + "<br/>");// 15
+document.write(str * 2 + "<br/>");// 40
+document.write(str / 2 + "<br/>");// 10
+
+
+
